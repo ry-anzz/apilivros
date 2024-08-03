bbc45ceb0b3d4c71333429a22cb2ea7238e7effd
unknown <you@example.com>
2024-08-02 22:55:28 -0300

    funções da api

---
+++ app/Http/Controllers/tbl_Livros.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class tbl_Livros extends Controller
+{
+    
+
+    public function index(){
+
+    }
+
+    public function show(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function update(){
+
+    }
+
+
+    public function destroy(){
+
+    }
+
+
+
+}
---
+++ app/Models/tbl_Livros.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tbl_Livros extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nomeLivro',
+        'generoLivro',
+        'anoLivro',
+    ];
+}
